2ff465fb11445f02222f72299d80cd2f23e69fef
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-22 13:33:46 -0600

    S9 - useEffect - Precauciones

--- src/useEffect/Message.jsx
+++ src/useEffect/Message.jsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({x: 0, y: 0});
+
   useEffect(() => {
     console.log('Message mounted');
+
+    const onMouseMove = ({x, y}) => {
+      // const coords = { x, y };
+      console.log({x, y});
+      setCoords({ x, y });
+    }
+    window.addEventListener('mousemove', onMouseMove);
     
   
     return () => {
+      window.removeEventListener('mousemove', onMouseMove);
       console.log('Message unmounted');
     }
   }, [])
@@ -14,6 +24,7 @@ export const Message = () => {
   return (
     <>
         <h3>Usuario ya existe</h3>
+        { JSON.stringify(coords) }
     </>
   )
 }
