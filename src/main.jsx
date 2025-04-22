7bed21ce5752afe340fc1e2773a46403aee9fbd0
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-22 09:10:11 -0600

    S9 - useCounter - CustomHook

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp'
 import { Counter } from './useState/Counter'
+import { CounterWithCustomHook } from './useState/CounterWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
+    {/* <Counter /> */}
+    <CounterWithCustomHook />
   </StrictMode>,
 )
